aa7d4261fa340dfabdb6dfc98bd737a844e14361
glrmrissi <guilhermerissi04112006@gmail.com>
2026-02-18 15:25:14 -0300

    feat: implement authentication interceptor and enhance app initialization with dark mode support

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { PLATFORM_ID, inject } from "@angular/core";
 import { isPlatformBrowser } from "@angular/common";
-import { firstValueFrom, map, Observable } from "rxjs";
+import { catchError, firstValueFrom, map, Observable } from "rxjs";
 import environment from "../shared/environments/environment.local";
 
 @Injectable({
@@ -10,16 +10,6 @@ import environment from "../shared/environments/environment.local";
 })
 
 export class ApiService {
-    private token: string | null = null;
-    private platformId = inject(PLATFORM_ID);
-
-    private getToken(): string | null {
-        if (isPlatformBrowser(this.platformId)) {
-            this.token = localStorage.getItem('x_access_token');
-        }
-        return this.token;
-    }
-
     constructor(
         private http: HttpClient,
     ) { }
@@ -28,63 +18,47 @@ export class ApiService {
         return environment.apiUrl;
     }
 
-    getV1<T = any>(endpoint: string, params: any, extraHeaders?: { }, noAuth = true): Promise<T> {
+    getV1<T = any>(endpoint: string, params: any, extraHeaders?: {}, noAuth = true): Promise<T> {
         const url = `${this.getBaseUrl()}/${endpoint}`;
-        const token = this.getToken();
         const headers: Record<string, string> = {
             ...extraHeaders
         };
-        if (token) {
-            headers['x_access_token'] = `${token}`;
-        }
-        return firstValueFrom(this.http.get<T>(url, { params: params, headers, withCredentials: true }).pipe(map((res: T) => res)));
+        return firstValueFrom(
+            this.http.get<T>(url, { params: params, headers, withCredentials: true }).pipe(
+                map((res: T) => res)
+            )
+        );
     }
 
     postV1<T = any>(endpoint: string, body: any, extraHeaders?: {}, noAuth = true): Promise<T> {
         const url = `${this.getBaseUrl()}/${endpoint}`;
-        const token = this.getToken();
         const headers: Record<string, string> = {
             ...extraHeaders
         };
-        if (token) {
-            headers['x_access_token'] = `${token}`;
-        }
         return firstValueFrom(this.http.post<T>(url, body, { headers, withCredentials: true }).pipe(map((res: T) => res)));
     }
 
     patchV1<T = any>(endpoint: string, body: any, extraHeaders?: {}, noAuth = true): Promise<T> {
         const url = `${this.getBaseUrl()}/${endpoint}`;
-        const token = this.getToken();
         const headers: Record<string, string> = {
             ...extraHeaders
         };
-        if (token) {
-            headers['x_access_token'] = `${token}`;
-        }
         return firstValueFrom(this.http.patch<T>(url, body, { headers, withCredentials: true }).pipe(map((res: T) => res)));
     }
 
     putV1<T = any>(endpoint: string, body: any, extraHeaders?: {}, noAuth = true): Promise<T> {
         const url = `${this.getBaseUrl()}/${endpoint}`;
-        const token = this.getToken();
         const headers: Record<string, string> = {
             ...extraHeaders
         };
-        if (token) {
-            headers['x_access_token'] = `${token}`;
-        }
         return firstValueFrom(this.http.put<T>(url, body, { headers, withCredentials: true }).pipe(map((res: T) => res)));
     }
 
     deleteV1<T = any>(endpoint: string, extraHeaders?: {}, noAuth = true): Promise<T> {
         const url = `${this.getBaseUrl()}/${endpoint}`;
-        const token = this.getToken();
         const headers: Record<string, string> = {
             ...extraHeaders
         };
-        if (token) {
-            headers['x_access_token'] = `${token}`;
-        }
         return firstValueFrom(this.http.delete<T>(url, { headers, withCredentials: true }).pipe(map((res: T) => res)));
     }
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  private platformId = inject(PLATFORM_ID);
+  
   protected readonly title = signal('front-end');
+
+  ngOnInit() {
+    if (!isPlatformBrowser(this.platformId)) {
+      return;
+    }
+
+    const theme = localStorage.getItem('theme');
+    const isDarkMode = theme !== 'true';
+    
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark-mode');
+    } else {
+      document.documentElement.classList.remove('dark-mode');
+    }
+  }
 }
