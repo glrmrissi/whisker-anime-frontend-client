58563f55d1a57ff17f62cf4be773d79bc21e5b0e
glrmrissi <guilhermerissi04112006@gmail.com>
2026-02-28 21:32:57 -0300

    refactor: more performance to home component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,131 +1,99 @@
+import { Component, PLATFORM_ID, inject, signal, OnInit } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-import { Component, ChangeDetectorRef, AfterViewInit, PLATFORM_ID, inject, signal } from '@angular/core';
-import { HomeService } from './home.service';
 import { Header } from '../header/header';
 import { HeroSlide, HeroSliderComponent } from '../hero-slider/hero-slider';
-import { AnimeItem, Carousel } from './components/carousel';
+import { Carousel } from './components/carousel';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { HomeService } from './home.service';
 import { faBolt, faWater } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-home',
   imports: [Header, HeroSliderComponent, Carousel, FaIconComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home implements AfterViewInit {
-
+export class Home implements OnInit {
   private readonly platformId = inject(PLATFORM_ID);
+  private readonly homeService = inject(HomeService);
 
-  protected faBolt = faBolt
-  
-  protected faWater = faWater
+  heroSlides = signal<HeroSlide[]>([]);
+  latestRelease = signal<any[]>([]);
+  moreViewers = signal<any[]>([]);
+  ovasRelease = signal<any[]>([]);
+  averageRating = signal<any[]>([]);
+  isLoading = signal<boolean>(true);
 
-  constructor(
-    private readonly homeService: HomeService,
-    private cdr: ChangeDetectorRef
-  ) { }
+  protected faBolt = faBolt;
+  protected faWater = faWater;
 
-  animeTrending: AnimeItem[] = [];
-  latestRelease: any[] = [];
-  ovasRelease: any[] = [];
-  averageRating: any[] = [];
-  favoriteAnimes: any[] = [];
-
-  async ngAfterViewInit() {
-    if (!isPlatformBrowser(this.platformId)) return;
-
-    await this.fetchTrendingAnime();
-    await this.getAnimeTrending()
-  }
-
-  heroSlides = signal<HeroSlide[]>([])
-
-  async getAnimeTrending() {
-    try {
-      const data = await this.homeService.getAnimeTrending();
-
-      const heroSlides = (data?.data || []).map((anime: any) => {
-        const year = new Date(anime.attributes.startDate).getFullYear();
-        return {
-          id: anime.id,
-          badgeText: 'On the rise',
-          title: anime.attributes.canonicalTitle || '',
-          rating: anime.attributes.averageRating
-            ? parseFloat((parseFloat(anime.attributes.averageRating) / 10).toFixed(1))
-            : 0,
-          subtype: anime.attributes.subtype || '',
-          year: year,
-          description: anime.attributes.description || '',
-          backgroundUrl: anime.attributes.coverImage?.large || ''
-        };
-      });
-
-      this.heroSlides.set(heroSlides);
-      this.cdr.markForCheck();
-    } catch (error) {
-      this.heroSlides.set([]);
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.fetchAllData();
     }
   }
 
-  async fetchTrendingAnime() {
+  private async fetchAllData() {
+    this.isLoading.set(true);
+    
     try {
-      const data = await this.homeService.getAnimePagination('1', '20', '-createdAt', 'TV,OVA,ONA,MOVIE,SPECIAL');
-      this.latestRelease = (data?.data || []).map((anime: any) => ({
-        id: anime.id,
-        coverImage: anime.attributes.posterImage?.large || anime.attributes.coverImage?.large || '',
-        canonicalTitle: anime.attributes.canonicalTitle || '',
-        description: anime.attributes.description || '',
-        link: anime.attributes.slug || '',
-        subtype: [anime.attributes.subtype || '']
-      }));
-      this.cdr.markForCheck();
-    } catch (error) {
-      this.latestRelease = [];
+      await Promise.all([
+        this.loadHero(),
+        this.loadLatest(),
+        this.loadMoreViewers(),
+        this.loadOvas(),
+        this.loadTopRated()
+      ]);
+    } finally {
+      this.isLoading.set(false);
     }
+  }
 
-    try {
-      const data = await this.homeService.getAnimePagination('1', '20', '-createdAt', 'OVA');
-      this.ovasRelease = (data?.data || []).map((anime: any) => ({
-        id: anime.id,
-        coverImage: anime.attributes.posterImage?.large || anime.attributes.coverImage?.large || '',
-        canonicalTitle: anime.attributes.canonicalTitle || '',
-        description: anime.attributes.description || '',
-        link: anime.attributes.slug || '',
-        subtype: [anime.attributes.subtype || '']
-      }));
-      this.cdr.markForCheck();
-    } catch (error) {
-      this.ovasRelease = [];
-    }
+  private mapAnimeData(data: any) {
+    return (data?.data || []).map((anime: any) => ({
+      id: anime.id,
+      coverImage: anime.attributes.posterImage?.large || anime.attributes.coverImage?.large || '',
+      canonicalTitle: anime.attributes.canonicalTitle || '',
+      description: anime.attributes.description || '',
+      link: anime.attributes.slug || '',
+      subtype: [anime.attributes.subtype || '']
+    }));
+  }
 
-    try {
-      const data = await this.homeService.getAnimePagination('1', '20', '-averageRating', 'TV,OVA,ONA,MOVIE,SPECIAL');
-      this.averageRating = (data?.data || []).map((anime: any) => ({
-        id: anime.id,
-        coverImage: anime.attributes.posterImage?.large || anime.attributes.coverImage?.large || '',
-        canonicalTitle: anime.attributes.canonicalTitle || '',
-        description: anime.attributes.description || '',
-        link: anime.attributes.slug || '',
-        subtype: [anime.attributes.subtype || '']
-      }));
-      this.cdr.markForCheck();
-    } catch (error) {
-      this.averageRating = [];
-    }
+  private async loadHero() {
+    const data = await this.homeService.getAnimeTrending();
+    const mapped = (data?.data || []).map((anime: any) => ({
+      id: anime.id,
+      badgeText: 'On the rise',
+      title: anime.attributes.canonicalTitle || '',
+      rating: anime.attributes.averageRating 
+        ? parseFloat((parseFloat(anime.attributes.averageRating) / 10).toFixed(1)) 
+        : 0,
+      subtype: anime.attributes.subtype || '',
+      year: new Date(anime.attributes.startDate).getFullYear(),
+      description: anime.attributes.description || '',
+      backgroundUrl: anime.attributes.coverImage?.large || ''
+    }));
+    this.heroSlides.set(mapped);
+  }
 
-    try {
-      const data = await this.homeService.getFavoriteAnimes();
-      this.favoriteAnimes = (data?.data || []).map((anime: any) => ({
-        id: anime.id,
-        coverImage: anime.attributes.posterImage?.large || anime.attributes.coverImage?.large || '',
-        canonicalTitle: anime.attributes.canonicalTitle || '',
-        description: anime.attributes.description || '',
-        link: anime.attributes.slug || '',
-        subtype: [anime.attributes.subtype || '']
-      }));
-      this.cdr.markForCheck();
-    } catch (error) {
-      this.favoriteAnimes = [];
-    }
+  private async loadLatest() {
+    const data = await this.homeService.getAnimePagination('1', '20', '-createdAt', 'TV,OVA,ONA,MOVIE,SPECIAL');
+    this.latestRelease.set(this.mapAnimeData(data));
+  }
+
+  private async loadMoreViewers() {
+    const data = await this.homeService.getAnimePagination('1', '20', '-userCount', 'TV,OVA,ONA,MOVIE,SPECIAL');
+    this.moreViewers.set(this.mapAnimeData(data));
+  }
+
+  private async loadOvas() {
+    const data = await this.homeService.getAnimePagination('1', '20', '-createdAt', 'OVA');
+    this.ovasRelease.set(this.mapAnimeData(data));
+  }
+
+  private async loadTopRated() {
+    const data = await this.homeService.getAnimePagination('1', '20', '-averageRating', 'TV,OVA,ONA,MOVIE,SPECIAL');
+    this.averageRating.set(this.mapAnimeData(data));
   }
 }
